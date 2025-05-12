4fbea6dd1636b2e12ffd086f8c97822f4dc24055
harismemon13 <memonharis042gmail.com>
2025-05-13 00:20:18 +0500

    first assignment is completed

--- app.js
+++ app.js
@@ -1,2 +1,56 @@
-var fullName ("HARIS MEMON");
-alert(fullName)
+// =========== ASSIGNMENT 19 = part 1  STARTED===============
+// ===========Chapter 1 start ===========
+
+// alert("Hello Javascript")
+
+// alert("wellcome to JS land\nhappy coding")
+
+// alert("wellcome to JS land")
+// alert("happy coding\nprevent this page from creating additional dialogs")
+
+// alert("hello... i can run javascript through my web browser's console")
+
+// xxxxxxxxxxxxx chapter 1 end xxxxxxxxxxxx
+
+//========== Chapter 2 start =============
+
+// var myName = "HARIS MEMON"
+// alert(myName);
+
+// var myFavColour
+// myFavColour = "black"
+// alert(myFavColour)
+
+//xxxxxxxxxxx Chapter 2 end xxxxxxxxxxxxxxx
+
+//============chapter 3 start ===========
+
+// var age = "i am 17 years old"
+// alert(age)
+
+// var visting = "you have vist my site 14 times"
+// alert(visting)
+
+//xxxxxxxxxxx chapter 3 end xxxxxxxxxxxxxxx
+//xxxxxxxxxxx ASSIGNMENT 19 = PART 1 COMPLETED xxxxxxxxxxxxxxxxx
+
+//===========ASSIGNMENT 19 = PART 2 STARTED ==============
+//===========chapter 1 start ==============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
