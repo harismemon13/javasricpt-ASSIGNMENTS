31ebe25406e30a54eb5c80eac2c46e6e8a1a0679
harismemon13 <memonharis042gmail.com>
2025-05-13 01:03:06 +0500

    assignment 19 part 2 completed

--- app.js
+++ app.js
@@ -37,18 +37,51 @@
 //===========ASSIGNMENT 19 = PART 2 STARTED ==============
 //===========chapter 1 start ==============
 
+// var firstName ="haris"
+// var lastName ="memon"
+// var email = "memonharis.gmail.com"
+// var phoneNumber = 03082971481
+// var password = "harismemon13"
 
+// alert (firstName);
+// alert (lastName);
+// alert (email);
+// alert (phoneNumber);
+// alert (password);
 
+// alert("You're learning JavaScript!");
 
+//xxxxxxxxxchapter 1 end xxxxxxxxxx
 
+//=========chapter 2 start =========
 
+// var fullName ="haris memon"
+// var teamName ="cricketgroup"
+// alert (teamName)
 
+// var bestMan = "charlie"
 
+//xxxxxxxxxxchapter 2 end xxxxxxxxx
 
+//========chapter 3 start ==========
 
+// var caseQty = 144
+// var num = 9
+// var sum = caseQty + num
+// alert(sum)
 
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal )
 
+//xxxxxxxxxchapter 3 end xxxxxxxxxxx
 
+//xxxxxxxxxxxxASSIGNMENT 19 PART 2 completed xxxxxxxxxxxxxx
+
+//============ASSIGNMENT 20 PART 1 STARTED =============
+
+//============chapter 4 start ============
 
 
 
